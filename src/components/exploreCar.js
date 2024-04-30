13a84ba690528ca5e46e1c4d72a414d65876af03
Weizhou Zhou <zhou.weizh@northeastern.edu>
2024-04-29 21:57:52 -0700

    Check

--- src/components/exploreCar.js
+++ src/components/exploreCar.js
@@ -4,28 +4,28 @@ const exploreCars = [
         Engine: " 3.0L L6",
         Acceleration: " 3.4s",
         HP: " 503Hp/579Nm",
-        imagePath: "/BimmerClub/pics/BMW_1_Portrait.jpg"
+        imagePath: "pics/BMW_1_Portrait.jpg"
     },
     {
         model: "M4",
         Engine: " 3.0L L6",
         Acceleration: " 3.4s",
         HP: " 523Hp/586Nm",        
-        imagePath: "/BimmerClub/pics/BMW_2_Portrait.jpg"
+        imagePath: "pics/BMW_2_Portrait.jpg"
     },
     {
         model: "M5",
         Engine: " 4.4L V8",
         Acceleration: " 3.2s",
         HP: " 627Hp/750Nm",
-        imagePath: "/BimmerClub/pics/BMW_3_Portrait.jpg"
+        imagePath: "pics/BMW_3_Portrait.jpg"
     },
     {
         model: "M8",
         Engine: " 4.4L V8",
         Acceleration: " 3.0s",
         HP: " 617Hp/750Nm",
-        imagePath: "/BimmerClub/pics/BMW_M8.jpg"
+        imagePath: "pics/BMW_M8.jpg"
     }
 ]
 
